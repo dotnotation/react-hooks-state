54df640abca0981634771f6627ab485b40509da5
dotnotation <dotinmotionproductions@gmail.com>
2022-04-18 20:26:21 -0400

    added notes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,8 @@ function Counter() {
   function increment() {
     setCount((currentCount) => currentCount + 1);
     setCount((currentCount) => currentCount + 1);
+    // using a callback function so that the current state is passed instead of calling it async.
+    // if you didn't use a callback, the count would only increase by 1, not 2 so effectively only one setCount would be read
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
